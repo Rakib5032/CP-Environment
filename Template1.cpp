f582d7bbc1b6f531e299c576f9a767b12cbc50c7
rakib5032 <rabbi15-5032@diu.edu.bd>
2023-11-24 03:51:29 +0600

    Template1 added

---
+++ Template1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define forn(i, n) for(int i = 0; i < n; ++i)
+
+typedef long long ll;
+typedef pair<int, int> pii;
+const int INF = 1e9;
+const int N = 1e5+10;
+
+
+void solve() {
+    
+}
+
+int main() {
+    fastio;
+
+
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+
+
+    return 0;
+}
